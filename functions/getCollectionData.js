9c7af055fa0ce41d2e477ab2a8eca3331bc7feb1
Jorge Arreola <jorgitoarreolas@gmail.com>
2021-01-06 11:06:34 -0600

    try to enable cors on getCollectionData

--- functions/getCollectionData.js
+++ functions/getCollectionData.js
@@ -51,6 +51,12 @@ exports.handler = async (event) => {
 
     return {
         statusCode: 200,
+        headers: {
+            /* Required for CORS support to work */
+            'Access-Control-Allow-Origin': '*',
+            /* Required for cookies, authorization headers with HTTPS */
+            'Access-Control-Allow-Credentials': true
+          },
         body: JSON.stringify(await getPageData(id), null, 2),
       }
 }
